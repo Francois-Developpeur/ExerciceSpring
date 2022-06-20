8d20ac7cd65a141609b37966d32caa71547e403a
Francois-Developpeur <francoisbierbaum@gmail.com>
2022-06-20 16:49:16 +0200

    fin de journée 20/06/2022

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/BaseballCoach.java
@@ -0,0 +1,13 @@
+package com.luv2code.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "hit the ball 150times in a row";
+	}
+
+}
